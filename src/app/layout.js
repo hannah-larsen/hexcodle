9fd0a15ec26aa179888aef334d86273835a37f86
Ekimerton <ekim0252@gmail.com>
2023-10-02 17:29:51 -0400

    Fixed GTAG

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Script id="google-tag-manager" strategy="worker">
+      <Script id="google-tag-manager" strategy="afterInteractive">
         {`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
