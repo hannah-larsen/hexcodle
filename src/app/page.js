4e74d0028882d03562579251959fbf966e1817a5
Ekimerton <ekim0252@gmail.com>
2023-10-03 00:22:31 -0400

    TODO: fix dynamic SSR

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,8 @@ import HexcodleGame from "./HexcodleGame";
 import HexInfoModal from "./components/HexInfoModal";
 import { generateRandomHexcode } from "./utils";
 
+// Fix static generation
+
 export default function Home() {
   const randColor = generateRandomHexcode();
   return <HexcodleGame targetColor={randColor} />;
