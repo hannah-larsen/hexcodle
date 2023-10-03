367c3adc0bd7bfef4cf85840140ce323c83e92de
Ekimerton <ekim0252@gmail.com>
2023-10-03 00:55:41 -0400

    Added cookies to force dynamic rendering

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import HexcodleGame from "./HexcodleGame";
-import HexInfoModal from "./components/HexInfoModal";
+import { cookies } from "next-headers";
 import { generateRandomHexcode } from "./utils";
 
 // Fix static generation
 
 export default function Home() {
+  // Added to force dynamic rendering
+  const cookieStore = cookies();
   const randColor = generateRandomHexcode();
   return <HexcodleGame targetColor={randColor} />;
 }
