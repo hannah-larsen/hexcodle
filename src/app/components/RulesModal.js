48fe9a3d7412825e24397d5e1526efc584c9c974
Hannah Larsen <18hkl@queensu.ca>
2023-10-29 17:39:35 -0400

    fixed esc characters

--- src/app/components/RulesModal.js
+++ src/app/components/RulesModal.js
@@ -16,17 +16,17 @@ export default function RulesModal({ isOpen, setIsOpen }) {
         >
         <p>
             You will have 5 tries to correctly guess the hex code of the colour
-            displayed on screen in the "target" box. After each guess, the colour of the hex code
-            you entered will be displayed in the "your guess" box.
+            displayed on screen in the <i>target</i> box. After each guess, the colour of the hex code
+            you entered will be displayed in the <i>your guess</i> box.
         </p>
         <p>
-            There will be symbols that pop up in the "guesses" section that indicate the closeness
+            There will be symbols that pop up in the <i>guesses</i> section that indicate the closeness
             of your guess. Use these to gauge your next guess! Here is what each symbol means:
         </p>
         <p>
             ✅ You got it! <br></br>
-            🔼 Guess higher (only off by 1-2) <br></br>
-            🔽 Guess lower (only off by 1-2) <br></br>
+            🔼 Guess higher (only off by 1 or 2) <br></br>
+            🔽 Guess lower (only off by 1 or 2) <br></br>
             ⏫ Guess way higher! (off by 3 or more) <br></br>
             ⏬ Guess way lower! (off by 3 or more) <br></br>
         </p>
