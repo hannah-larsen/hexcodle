ff691242d28f1acdd03ff1e1e3438260b48d151e
Ekimerton <ekim0252@gmail.com>
2024-01-09 01:30:44 -0500

    Made archive dyanmic again

--- src/app/archive/page.js
+++ src/app/archive/page.js
@@ -6,6 +6,7 @@ import {
   generateHexcode,
   getHexcodleNumber,
 } from "../utils";
+import { headers } from "next/headers";
 
 export async function load() {
   const totalHexcodleItems = getHexcodleNumber();
@@ -28,6 +29,7 @@ export async function load() {
 }
 
 export default async function Archive() {
+  const headersList = headers();
   const panelsData = await load();
   return <ArchivePage panelsData={panelsData} />;
 }
