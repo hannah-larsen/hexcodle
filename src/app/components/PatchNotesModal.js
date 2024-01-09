daeeeddd38ab8d44807e1c366f8d5c7cde829408
Hannah Larsen <18hkl@queensu.ca>
2024-01-09 14:39:59 -0500

    added patch notes and minor style changes
    Co-authored-by: Ekim <ekim0252@gmail.com>

--- src/app/components/PatchNotesModal.js
+++ src/app/components/PatchNotesModal.js
@@ -4,7 +4,7 @@ export default function PatchNotesModal({ isOpen, setIsOpen }) {
   return (
     <Modal
       okButtonProps={{ style: { backgroundColor: "var(--primary)" } }}
-      title={<center>❄️Patch Notes 12.12.23❄️</center>}
+      title={<center>☃️Patch Notes 1.9.24☃️</center>}
       open={isOpen}
       onOk={() => {
         setIsOpen(false);
@@ -14,21 +14,23 @@ export default function PatchNotesModal({ isOpen, setIsOpen }) {
       }}
       cancelButtonProps={{ style: { display: "none" } }}
     >
-        <ul>
+      <ul>
         <li>
-            We see your feedback and we love to improve Hexcodle wherever we can! Thank you for playing
-            and taking the time to give us insightful ideas. If you left us feedback in our feedback form and we 
-            have not addressed it, we will get to it soon!
+          We see your feedback and we love to improve Hexcodle wherever we can!
+          Thank you for playing and taking the time to give us insightful ideas.
+          If you left us feedback in our feedback form and we have not addressed
+          it, we will get to it soon!
         </li>
         <li>
-            <strong>General updates:</strong> Updated our backrgound/theme colour to be a bit more calm on the eyes.
+          <strong>Archive:</strong> You asked, we delivered! Introducing the
+          Hexcodle Archive - click the new button at the top of our navbar to
+          access all our previous puzzles!
         </li>
         <li>
-            <strong>Share button updates:</strong> When you complete the game, you can now view the name of the daily colour! 
-            We have a mascot now! When you complete the game, you will see the Hexcodle parrot based on your results. What should we name it?
-        </li>
-        <li>
-          <strong>Upcoming updates:</strong> Archive, stats, and much more! Stay tuned...
+          <strong>Hexcodle Parrot Naming Contest:</strong> Thank you to everyone
+          who entered our contest. Congrats to <strong>Olivia</strong> with
+          their submission! The Hexcodle parrot is now officially named{" "}
+          <strong>Hexavier</strong>!
         </li>
       </ul>
     </Modal>
