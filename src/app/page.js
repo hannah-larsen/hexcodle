c952dfb2c422c766f5ce0ddd7b172e2bcc0096ee
Ekimerton <ekim0252@gmail.com>
2024-04-03 02:40:04 -0400

    Fixed mini metadata

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,12 @@ import HexcodleGame from "./HexcodleGame";
 import { cookies } from "next/headers";
 import { generateHexcode, getColorName, getHexcodleNumber } from "./utils";
 
+export const metadata = {
+  title: "Hexcodle Mini - The Bite Sized Hexcode Guessing Game!",
+  description:
+    "Can you guess today's hexcode color? Challenge your color instincts and decode the hexcode with Hexcodle! Inspired by Wordle, but for color enthusiasts.",
+};
+
 export default async function Home() {
   // Added to force dynamic rendering
   const cookieStore = cookies();
