48a5a87d186f953e55d3967a779beb1df584b59f
Ekim Karabey <ekim0252@gmail.com>
2026-02-15 19:41:17 -0800

    Added lip to bottom of navbar

--- src/app/components/NavbarClient.jsx
+++ src/app/components/NavbarClient.jsx
@@ -52,14 +52,13 @@ export default function NavbarClient({ hexcodleNumber, miniNumber }) {
     const noiseTexture = `url("data:image/svg+xml,%3Csvg viewBox='0 0 200 200' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.20' numOctaves='3' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)' opacity='0.20'/%3E%3C/svg%3E")`;
 
     return (
-        <header className="fixed top-0 w-full z-50 drop-shadow-xl">
+        <header className="fixed top-0 w-full z-50 drop-shadow-xl border-b-2 border-black">
             {/* Top Section */}
             <div
                 className="bg-blue-900 w-full relative z-20"
                 style={{
                     backgroundImage: noiseTexture,
                     backgroundSize: "75px 75px",
-                    borderBottom: "1px solid rgba(255, 255, 255, 0.1)"
                 }}
             >
                 <div className="flex flex-row items-center justify-between p-2 min-h-[60px] max-w-7xl mx-auto w-full">
